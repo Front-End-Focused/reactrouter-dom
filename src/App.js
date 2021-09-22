157e17c8222720460dae90e3e0e3d6f810f6fc91
Viktorya <smirnovaviktorya1@gmail.com>
2021-09-22 21:07:27 +0300

    2021_Viktorya

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Assets from "./components/Assets";
 import Guide from "./components/Guide";
 import Projects from "./components/Projects";
 import PrivatePage from "./components/PrivatePage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Login from "./components/Login";
+
 
 function App() {
   return (
@@ -53,7 +56,8 @@ function App() {
                 <Route path="/guide" component={Guide} />
                 <Route path="/assets" component={Assets} />
                 <Route path="/projects" component={Projects} />
-                <Route path="/private" component={PrivatePage} />
+                <Route path="/login" component={Login} />
+                <ProtectedRoute path='/private' component={PrivatePage} isLoggedIn={true} />
                 {/* <Route to="/">
                   <h1 className="display-5">Welcome to Manage!</h1>
                 </Route> */}
